9002c052dc2d06ab2ba26380939c8d703b9e1e41
masjedabdullahbinomar-max <masjed.abdullah.bin.omar@gmail.com>
2026-09-14 21:15:02 +0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+window.FIREBASE_CONFIG = {
+  apiKey:            "ضع-API-KEY-هنا",
+  authDomain:        "your-project.firebaseapp.com",
+  databaseURL:       "https://your-project-default-rtdb.firebaseio.com",
+  projectId:         "your-project",
+  storageBucket:      "your-project.appspot.com",
+  messagingSenderId: "000000000000",
+  appId:             "1:000000000000:web:xxxxxxxxxxxxxxxxxxxxxx"
+};
+
+window.SCREEN_ID = "IBRI-MOSQUE-TV-001";
+window.APP_VERSION = "1.0.0";
